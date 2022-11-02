53fdab7dc2c3812ea877309c943e8d5867b5d669
dylandupasquier <dylandupasquier@gmail.com>
2022-11-01 22:14:03 -0700

    add button and style description

--- src/lib/Projects/ProjectItem.svelte
+++ src/lib/Projects/ProjectItem.svelte
@@ -2,6 +2,7 @@
 	import { fade } from 'svelte/transition';
 	import { onMount } from 'svelte';
 	import type { Project } from '$lib/data/projects';
+	import WideButton from '$lib/WideButton.svelte';
 
 	export let project: Project;
 
@@ -19,8 +20,14 @@
 	});
 
 	$: more = '';
-	const toggleMore = () => {
-		more = 'shown' || '';
+	const descShown = () => {
+		more = 'shown';
+		console.log(more);
+	};
+
+	const descHidden = () => {
+		more = '';
+		console.log(more);
 	};
 </script>
 
@@ -40,17 +47,20 @@
 		<div class="when">
 			{project.when}
 		</div>
-		<button class="more-less" on:click={toggleMore}> More </button>
+		<button class="more-btn" on:click={descShown}>Details</button>
 	</div>
-	<ul class="tech">
-		{#each project.tech as tech}
-			<li class="tech-item" in:fade={{ delay: 400, duration }} out:fade={{ duration }}>
-				{tech}
-			</li>
-		{/each}
-	</ul>
 	<div class="description {more}" in:fade={{ delay: 400, duration }} out:fade={{ duration }}>
+		<div class="less-btn">
+			<WideButton onClick={descHidden}>Close</WideButton>
+		</div>
 		{project.description}
+		<ul class="tech">
+			{#each project.tech as tech}
+				<li class="tech-item" in:fade={{ delay: 400, duration }} out:fade={{ duration }}>
+					{tech}
+				</li>
+			{/each}
+		</ul>
 	</div>
 </div>
 
@@ -61,12 +71,6 @@
 		position: relative;
 		cursor: pointer;
 		max-height: 30rem;
-	}
-
-	@media (min-width: 768px) {
-		.gallery-item:hover .tech {
-			transform: scaleY(100%);
-		}
 	}
 
 	.gallery-image {
@@ -107,11 +111,13 @@
 		text-align: center;
 		top: 0;
 		bottom: 0;
+		left: 0;
+		right: 0;
 		background-color: rgba(0, 0, 0);
 		color: #fff;
 		font-size: 1.2rem;
 		padding: 0.5rem;
-		border-radius: 0.2rem;
+		border-radius: 0.3rem;
 		transition: transform 0.6s;
 		transform-origin: right;
 		transform: scaleX(0);
@@ -121,10 +127,10 @@
 		transform: scaleX(100%);
 	}
 
-	.more-less {
+	.more-btn {
 		color: colors.$purple;
-		font-size: 0.8rem;
-		font-weight: 700;
+		font-size: 0.9rem;
+		font-weight: 800;
 		padding: 0.5rem;
 		margin-top: 0.5rem;
 		background-color: rgba(0, 0, 0);
@@ -137,6 +143,13 @@
 		}
 	}
 
+	.less-btn {
+		position: absolute;
+		top: 0;
+		right: 0;
+		width: 100%;
+	}
+
 	.tech {
 		position: absolute;
 		bottom: 0;
@@ -147,11 +160,7 @@
 		flex-wrap: wrap;
 		list-style: none;
 		padding: 0;
-		background: rgba(0, 0, 0, 0.5);
-		border-radius: 0 0 0.3rem 0.3rem;
-		transform-origin: bottom;
-		transform: scaleY(0);
-		transition: all 0.6s;
+		margin-bottom: 1rem;
 		.tech-item {
 			font-size: 0.8rem;
 			font-weight: 400;
@@ -172,6 +181,32 @@
 			&:nth-child(even) {
 				&:hover {
 					transform: scale(1.2) rotate(-5deg);
+				}
+			}
+		}
+
+		@media (max-width: 768px) {
+			.tech-item {
+				font-size: 0.7rem;
+				font-weight: 400;
+				text-align: center;
+				margin: 0.2rem;
+				padding: 0;
+				border-radius: 5px;
+				background: none;
+				box-shadow: none;
+				transition: none;
+				cursor: default;
+				transition: all 0.6s;
+				&:nth-child(odd) {
+					&:hover {
+						transform: none;
+					}
+				}
+				&:nth-child(even) {
+					&:hover {
+						transform: none;
+					}
 				}
 			}
 		}
---
+++ src/lib/WideButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	export let onClick: () => void;
+</script>
+
+<button on:click={() => onClick()}>
+	<slot />
+</button>
+
+<style lang="scss">
+	@use 'src/styles/imports/colors' as colors;
+
+	button {
+		background-color: colors.$gray;
+		border: none;
+		border-radius: 0.3rem 0.3rem 0 0;
+		color: colors.$white;
+		cursor: pointer;
+		font-size: 1.5rem;
+		font-weight: 600;
+		width: 100%;
+		height: fit-content;
+		padding: .3rem;
+		transition: all 0.2s ease-in-out;
+		&:hover {
+			background: colors.$white;
+			color: black;
+		}
+	}
+</style>
